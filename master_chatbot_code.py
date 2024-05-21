d5123d36c209f4099a6fb0f56e7374c395098678
Hanan Tabak <hanan.tabak@gmail.com>
2024-05-21 20:25:57 +0400

    Update master_chatbot_code.py

--- master_chatbot_code.py
+++ master_chatbot_code.py
@@ -120,7 +120,7 @@ unique_id = uuid4().hex[0:8]
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = f"LangSmith - AI Chatbot with Text to Speech - {unique_id}"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"] = "${{ secrets.LANGSMITH_KEY }}"
+os.environ["LANGCHAIN_API_KEY"] = LANGSMITH_KEY
 
 
 #=================
